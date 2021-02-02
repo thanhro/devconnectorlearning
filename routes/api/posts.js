3f998d77dbb58adb2b3af413e980433f7d13f4e4
tuan_pham_xuan <tuan.pham.xuan@ntq-solution.com.vn>
2021-02-02 18:06:02 +0700

    coding ahihi

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('User route'));
+router.get('/', (req, res) => res.send('Posts route'));
 
 module.exports = router;
