a97c0c2d6517eeff33f0daabd63c2fccfdf18f7d
thanhro <lethanh309@gmail.com>
2021-05-06 21:26:59 +0700

    coding jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const {check, validationResult} = require('express-validator')
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 
 router.post('/',
@@ -47,7 +49,22 @@ router.post('/',
 
         await user.save();
 
-        res.send('Users registered!!!')
+        const payload = {
+            user : {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret') , 
+            {expiresIn: 36000},
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token })
+            }
+        );
+
     } catch (err) {
         console.log(err.message);
         res.status(500).send('Server error');
